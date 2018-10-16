642005c0d3558b7d34983d0aff41f66aa0a2befd
Shadsa <zehava-bol@live.fr>
2018-10-16 17:22:09 +0200

    Update build under windows, add a sleep
    Sleep allow the script to ensure that mvnw alway finish before npm, ensuring that the browser sync will be avaible

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
 @REM The terminal should run as admin before trying to build, in order to avoid different issue with right management
 powershell -Command "npm install"
 powershell -Command "Start-Process .\mvnw"
+powershell -Command "Start-Sleep 10"
 powershell -Command "Start-Process npm start"
