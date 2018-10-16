7c28fa2ded65ff9019efc7acbbaad7c66ca0f969
Shadsa <zehava-bol@live.fr>
2018-10-16 17:11:30 +0200

    Update build command to include npm install under Windows
    due to the lack of npm install, the build was bugged on the DB access

--- build.cmd
+++ build.cmd
@@ -1,2 +1,4 @@
 @REM The terminal should run as admin before trying to build, in order to avoid different issue with right management
-powershell -Command "Start-Process .\mvnw | Start-Process npm start"
+powershell -Command "npm install"
+powershell -Command "Start-Process .\mvnw"
+powershell -Command "Start-Process npm start"
